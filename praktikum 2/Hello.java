67823e30f212c6bb389f09c354baca321d30af6e
hildaputrinabillah <hildaputrinabillah@student.telkomuniversity.ac.id>
2020-09-17 16:59:04 +0700

    Hello.java

---
+++ praktikum 2/Hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("Nama    : Hilda Putri Nabillah");
+        System.out.println("Nama    : 6706192086");
+        System.out.println("Kelas  : 43-03");
+    }
+    
+}
